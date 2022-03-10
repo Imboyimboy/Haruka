5917e56616671a4dc5fc3706957e9bffac992d9f
Imboyimboy <101313469+Imboyimboy@users.noreply.github.com>
2022-03-10 19:46:03 +0700

    Update config.js

--- pengaturan/config.js
+++ pengaturan/config.js
@@ -7,7 +7,7 @@ global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 // setting
 global.ownername ="Imboy"
 global.ownernumber = "6283809742883"
-global.botname = "Imboy Order PM"
+global.botname = "Imboy"
 global.thumbnail = fs.readFileSync("./settings/Haruka.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.lolkey = 'RIFQIBOTZ' //apikey
